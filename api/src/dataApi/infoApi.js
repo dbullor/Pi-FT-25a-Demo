4ce60c39d2d984f706db5779782928b1b0702c12
Diego Bullor <dbullor@hotmail.com>
2022-05-24 10:24:23 -0300

    nuevo commit

--- api/src/dataApi/infoApi.js
+++ api/src/dataApi/infoApi.js
@@ -7,20 +7,20 @@ const {Country} = require('../db');
 
 const infoApi = async () =>{
     try {
-        const urlCountries = await axios(`https://restcountries.com/v3/all`);
+        const urlCountries = await axios.get(`https://restcountries.com/v3/all`);
         urlCountries.data.map(async(country)=>{//hago esto para que no me traiga datos que no son pedidos en el readme
            await Country.findOrCreate({
                 where:{
                 id: country.cca3,
                 name: country.name.official,
                 img_flag: country.img_flag[0],
-                continent: country.continent,
+                continent: country.region,
                 capital: country.capital
                     ? country.capital[0]
                     :"This Capital isn't loaded.",
-                sub_region: country.sub_region
-                    ? country.sub_region
-                    : "This Sub Región isn't loaded.",
+                sub_region: country.subregion
+                    ? country.subregion
+                    : "This Subregion isn't loaded.",
                 area: country.area,
                 population: country.population,
 
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,24 @@
 const { Router } = require('express');
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+
+const allCountriesRoutes = require('./getCountriesRoute.js');
+const routeActivities = require ('./postActivityRoute.js');
+const countryById = require ('./getCountryIdRoute.js')
+const getActivities = require('./getActivityRoute.js');
 
 
 
 
 
 const router = Router();
+
+router.use('/countries', allCountriesRoutes);
+router.use('/countries', countryById);
+router.use('/countries', getActivities)
+router.use('/activity', routeActivities);
+
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
